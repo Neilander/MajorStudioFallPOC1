ddc2f6e4894612a39d75033eb71ded08543113e6
Neilander <yw6472@nyu.edu>
2024-11-08 11:24:23 -0500

    music

---
+++ MajorStudioFallPOC1/Assets/audioManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class audioManager : MonoBehaviour
+{
+    public AudioSource source;
+    // Start is called before the first frame update
+    void Start()
+    {
+        source.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
